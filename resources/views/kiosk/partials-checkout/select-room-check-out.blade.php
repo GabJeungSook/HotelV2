d84bb0b36bfa2a04372f821fa23895a0998891db
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2026-04-05 21:41:23 +0800

    change svg

--- resources/views/kiosk/partials-checkout/select-room-check-out.blade.php
+++ resources/views/kiosk/partials-checkout/select-room-check-out.blade.php
@@ -8,7 +8,7 @@
 <div class="mt-5">
   <div class="flex justify-center ">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-24 w-24">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h1.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5-.545M14.25 9.75a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Z" />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
     </svg>
   </div>
   <div class="flex justify-center mt-16">
