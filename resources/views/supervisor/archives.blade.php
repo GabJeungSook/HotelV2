4c4d4854852b0aef91077ff5dfb0842ccf5a416f
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2026-04-23 16:24:04 +0800

    update repor reidne done

--- resources/views/supervisor/archives.blade.php
+++ resources/views/supervisor/archives.blade.php
@@ -2,5 +2,32 @@
     @section('breadcrumbs')
         Archives
     @endsection
-    <livewire:supervisor.archives />
+
+    {{-- HOMI Logo --}}
+    <div class="flex justify-start px-8 py-4">
+        <img src="{{ asset('images/homiLogo.png') }}" class="h-10" alt="HOMI">
+    </div>
+
+    <div class="px-8 pb-8">
+        <livewire:back-office.archives />
+
+        <script>
+            function printOut(data) {
+                var mywindow = window.open('', '', 'height=1000,width=1000');
+                mywindow.document.write('<html><head>');
+                mywindow.document.write('<title></title>');
+                mywindow.document.write(`<link rel="stylesheet" href="{{ Vite::asset('resources/css/app.css') }}" />`);
+                mywindow.document.write('</head><body >');
+                mywindow.document.write(data);
+                mywindow.document.write('</body></html>');
+
+                mywindow.document.close();
+                mywindow.focus();
+                setTimeout(() => {
+                    mywindow.print();
+                    return true;
+                }, 1000)
+            }
+        </script>
+    </div>
 </x-supervisor-layout>
